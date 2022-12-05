bff3ef48858dd67115e4083e3516a7a6d6f82434
Calvin Loveland <calvin@loveland.dev>
2022-12-04 17:56:53 -0700

    automatically configure git

--- software/git.py
+++ software/git.py
@@ -4,8 +4,14 @@ import subprocess
 def setup_git():
     # Setup git
     commands = ["git config --global user.name 'Calvin Loveland'",
-                "git config --global user.email 'calvin@loveland.dev'"]
+                "git config --global user.email 'calvin@loveland.dev'"
+                "git config --global pull.rebase false",
+                "git config --global core.editor 'nvim'",
+                ]
     for command in commands:
-        subprocess.call(command, shell=True)
+        if subprocess.call(command, shell=True) != 0:
+            print("Error running command: " + command)
 
-def main():
+def main(sudo_password):
+    """Run the program."""
+    setup_git()
