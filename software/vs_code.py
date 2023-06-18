9fd16a988dd5c1536ff98b8a557031715be07259
Calvin Loveland <calvin@loveland.dev>
2023-06-17 19:28:27 -0600

    Maybe fix vs_code install?

--- software/vs_code.py
+++ software/vs_code.py
@@ -14,8 +14,10 @@ class VSCode(Software):
     @classmethod
     def pre_apt(cls):
         return [
-            "wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -",
-            'sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"',
+            "wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg",
+            "sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg",
+            """sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'""",
+            "rm -f packages.microsoft.gpg"
         ]
 
     @classmethod
